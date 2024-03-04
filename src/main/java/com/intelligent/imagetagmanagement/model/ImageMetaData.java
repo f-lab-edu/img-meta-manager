a9e1a699be45b23ded77b7e8185574dc42813465
DHa92 <han920415@gmail.com>
2024-03-04 22:07:16 +0900

    변수 타입별 설정

--- src/main/java/com/intelligent/imagetagmanagement/model/ImageMetaData.java
+++ src/main/java/com/intelligent/imagetagmanagement/model/ImageMetaData.java
@@ -3,6 +3,8 @@ package com.intelligent.imagetagmanagement.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,8 +20,17 @@ public class ImageMetaData {
     @Column(name = "metadata_key")
     private String key;
 
-    @Column(name = "metadata_value")
-    private String value;
+    @Column(name = "metadata_string_value")
+    private String stringValue;
+
+    @Column(name = "metadata_number_value")
+    private long numberValue;
+
+    @Column(name = "metadata_date_value")
+    private LocalDateTime dateValue;
+
+    @Column(name = "data_type", nullable = false)
+    private String type;
 
     @ManyToOne
     @JoinColumn(name = "image_id")
