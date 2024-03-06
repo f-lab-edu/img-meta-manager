b8bfef0cf6d1df3084f8ded96f0067613f8577a7
DHa92 <han920415@gmail.com>
2024-03-07 02:13:13 +0900

    테스트 코드 변경

--- src/test/java/com/intelligent/imagetagmanagement/ImageTagManagementApplicationTests.java
+++ src/test/java/com/intelligent/imagetagmanagement/ImageTagManagementApplicationTests.java
@@ -1,12 +1,11 @@
 package com.intelligent.imagetagmanagement;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ImageTagManagementApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
--- src/test/java/com/intelligent/imagetagmanagement/service/ImageServicesTest.java
+++ src/test/java/com/intelligent/imagetagmanagement/service/ImageServicesTest.java
@@ -5,90 +5,73 @@ import com.intelligent.imagetagmanagement.model.ImageData;
 import com.intelligent.imagetagmanagement.model.ImageMetaData;
 import com.intelligent.imagetagmanagement.model.SearchFilter;
 import com.intelligent.imagetagmanagement.repository.ImageRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.DisplayName;
+import com.intelligent.imagetagmanagement.repository.MetadataRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
-@Transactional
+@ActiveProfiles("local")
 public class ImageServicesTest {
 
     @Autowired
     private ImageRepository imageRepository;
 
     @Autowired
+    private MetadataRepository metadataRepository;
+
+    @Autowired
     private ImageServices imageServices;
 
-    private List<ImageMetaData> 샘플메타데이터() {
+
+    private void setSampleMetadataList(ImageData imageData) {
         List<ImageMetaData> resultMetadatas = new ArrayList<>();
-        ImageMetaData metadata = new ImageMetaData();
-        metadata.setKey("extension");
-        metadata.setStringValue("jpeg");
-        metadata.setType("string");
 
-        ImageMetaData metadata2 = new ImageMetaData();
-        metadata2.setKey("extension");
-        metadata2.setStringValue("jpeg");
-        metadata2.setType("string");
+        resultMetadatas.add(ImageMetaData.builder().key("extension").stringValue("jpeg").type("string").imageData(imageData).build());
+        resultMetadatas.add(ImageMetaData.builder().key("extension").stringValue("jpeg").type("string").imageData(imageData).build());
+        resultMetadatas.add(ImageMetaData.builder().key("fileSize").numberValue(1540L).type("number").imageData(imageData).build());
 
-        resultMetadatas.add(metadata);
-        resultMetadatas.add(metadata2);
-
-        return resultMetadatas;
+        metadataRepository.saveAll(resultMetadatas);
     }
 
-    @Test
-    @DisplayName("이미지 저장 및 조회")
-    void 이미지저장및조회() {
-        ImageData imageData = ImageData.builder().name("이미지").fileName("이미지.jpg").fileLocation("/이미지.jpg").metadata(샘플메타데이터()).build();
-        imageRepository.save(imageData);
+    @BeforeEach
+    public void beforeTest() {
+        List<ImageData> imageDataList = new ArrayList<>();
+        imageDataList.add(ImageData.builder().name("이미지").fileName("이미지.jpg").fileLocation("/이미지.jpg").build());
+        imageDataList.add(ImageData.builder().name("이미지1").fileName("이미지1.jpg").fileLocation("/이미지1.jpg").build());
+        imageDataList.add(ImageData.builder().name("이미지2").fileName("이미지2.jpg").fileLocation("/이미지2.jpg").build());
+        imageDataList.add(ImageData.builder().name("이미지3").fileName("이미지3.jpeg").fileLocation("/이미지3.jpeg").build());
+        imageRepository.saveAll(imageDataList);
 
-        Optional<ImageData> retrievedImage = imageRepository.findById(imageData.getUuid());
-
-        retrievedImage.ifPresent(value -> {
-            assertThat(value.getName()).isEqualTo("이미지");
-            assertThat(value.getUuid()).isNotBlank();
-            assertThat(value.getFileName()).isEqualTo("이미지.jpg");
-            assertThat(value.getFileLocation()).isEqualTo("/이미지.jpg");
-            assertThat(value.getMetadata()).isEqualTo(샘플메타데이터());
-
+        imageDataList.forEach(imageData -> {
+            setSampleMetadataList(imageData);
         });
-
-
     }
 
     @Test
     void findByKeyword() {
-        List<ImageData> imageDataList = new ArrayList<>();
-        imageDataList.add(ImageData.builder().name("이미지").fileName("이미지.jpg").fileLocation("/이미지.jpg").metadata(샘플메타데이터()).build());
-        imageDataList.add(ImageData.builder().name("이미지").fileName("이미지.jpg").fileLocation("/이미지.jpg").metadata(샘플메타데이터()).build());
-        imageDataList.add(ImageData.builder().name("이미지").fileName("이미지.jpg").fileLocation("/이미지.jpg").metadata(샘플메타데이터()).build());
-        imageDataList.add(ImageData.builder().name("이미지").fileName("이미지.jpeg").fileLocation("/이미지.jpeg").metadata(샘플메타데이터()).build());
-        imageRepository.saveAll(imageDataList);
-
-
-        assertThat(imageServices.findByKeyword("이미지").size()).isEqualTo(4);
+        assertThat(imageServices.findByKeyword("이미").size()).isEqualTo(4);
         assertThat(imageServices.findByKeyword("jpg").size()).isEqualTo(3);
         assertThat(imageServices.findByKeyword("none").size()).isEqualTo(0);
-
+//
         assertThat(imageServices.findByKeyword("100203").size()).isEqualTo(4);
     }
 
     @Test
     void searchByFilter() throws InvalidSearchException {
         List<SearchFilter> searchFilter = new ArrayList<>();
-        searchFilter.add(SearchFilter.builder().key("fileSize").keyword("150").operator("and").criteria("like").build());
-//        Map<String, String > searchFilter = new HashMap<>();
-//        searchFilter.put("fileSize", "150");
+        searchFilter.add(SearchFilter.builder().key("fileSize").keyword("0").operator("and").criteria("goe").valueType("number").build());
 
-        imageRepository.searchByFilter(searchFilter);
+        System.out.println(imageRepository.searchByFilter(searchFilter).size());
 
+        assertThat(imageRepository.searchByFilter(searchFilter).size()).isGreaterThan(3);
     }
 }
