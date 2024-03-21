4235eb5c8bde61fd106b4fe9000fdc374d43161f
DHa92 <han920415@gmail.com>
2024-03-21 19:07:49 +0900

    sqsClient 설정 추가

--- src/main/java/com/intelligent/imagetagmanagement/config/QueryDslConfig.java
+++ src/main/java/com/intelligent/imagetagmanagement/config/QueryDslConfig.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class QueryDslConfig {
@@ -16,4 +18,7 @@ public class QueryDslConfig {
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
     }
+
+    @Bean
+    public SqsClient sqsClient() {return SqsClient.builder().region(Region.AP_NORTHEAST_2).build();}
 }
