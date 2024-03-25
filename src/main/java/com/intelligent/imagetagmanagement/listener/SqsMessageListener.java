077ee0ae5c4a4e17b89fe877073cafef253eda3c
DHa92 <han920415@gmail.com>
2024-03-26 01:57:23 +0900

    예외 추가

--- src/main/java/com/intelligent/imagetagmanagement/listener/SqsMessageListener.java
+++ src/main/java/com/intelligent/imagetagmanagement/listener/SqsMessageListener.java
@@ -1,10 +1,13 @@
 package com.intelligent.imagetagmanagement.listener;
 
+import com.drew.imaging.jpeg.JpegProcessingException;
 import com.intelligent.imagetagmanagement.service.WorkflowQueueService;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @Slf4j
@@ -13,10 +16,15 @@ public class SqsMessageListener {
     WorkflowQueueService workflowQueueService;
 
     @SqsListener("${amazon.sqs.workflow.name}")
-    public void messageListener(String message) {
+    public void messageListener(String message) throws JpegProcessingException {
         log.info("message : {}", message);
 
+        try {
         workflowQueueService.doWorkflow(message);
+
+        } catch (IOException e){
+            log.error(e.getMessage());
+        }
 
     }
 }
